19e575aca15e37b99dca0f223588e741bd7a3b05
jurip89 <juri@Barbaras-iMac.local>
2023-02-09 16:40:03 +0100

    deleted import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { Home, ErrorPage, Details, RootLayout, HomeB } from "./pages";
+import { Home, ErrorPage, Details, RootLayout } from "./pages";
 import "./index.css";
 
 const router = createBrowserRouter([
