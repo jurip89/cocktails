7a40ba42fc4f309587f8792f3666104c42383639
jurip89 <juri@Barbaras-iMac.local>
2023-02-09 16:38:57 +0100

    edited exports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 export { default as Home } from './Home/Home';
-export { default as HomeB } from './Home/HomeB';
 export {default as ErrorPage} from './ErrorPage'
 export {default as Details} from './Details/Details'
 export {default as RootLayout} from './RootLayout'
